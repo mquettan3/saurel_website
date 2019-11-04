886f6d32dec4255fe796a45843f9078bc1ff2a58
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2019-11-04 10:40:33 -0500

    Building the foundation for the site.  Added Blog and Events page.  Working to fit in events and blog entries.

--- src/components/header.js
+++ src/components/header.js
@@ -1,32 +1,20 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
 import React from "react"
+import "../styles/main.scss"
 
 const Header = ({ siteTitle }) => (
-  <header
-    style={{
-      background: `rebeccapurple`,
-      marginBottom: `1.45rem`,
-    }}
-  >
-    <div
-      style={{
-        margin: `0 auto`,
-        maxWidth: 960,
-        padding: `1.45rem 1.0875rem`,
-      }}
-    >
-      <h1 style={{ margin: 0 }}>
-        <Link
-          to="/"
-          style={{
-            color: `white`,
-            textDecoration: `none`,
-          }}
-        >
-          {siteTitle}
-        </Link>
+  <header className="header">
+    <div className="container header-wrapper">
+      <h1 className="title">
+        <Link to="/">{siteTitle}</Link>
       </h1>
+      <ul className="navigation">
+        <li><Link to="/">Home</Link></li>
+        <li><Link to="/about">About</Link></li>
+        <li><Link to="/blog">Blog</Link></li>
+        <li><Link to="/events">Events</Link></li>
+      </ul>
     </div>
   </header>
 )
---
+++ src/pages/about.js
@@ -0,0 +1,13 @@
+import React from 'react'
+import Layout from "../components/layout"
+
+export default function AboutPage() {
+    return (
+        <Layout>
+            <div>
+                <h1>About Me</h1>
+                <p>Dolor elit deserunt laborum nulla. Consectetur culpa irure occaecat consequat ea culpa eiusmod ipsum ipsum. Sint et laboris do nisi elit tempor cillum nulla nostrud nostrud adipisicing irure ullamco.</p>
+            </div>
+        </Layout>
+    )
+}
