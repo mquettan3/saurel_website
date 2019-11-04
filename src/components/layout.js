031f79d63a1fb205f2f8bf100b62c2928c8fcc0f
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2019-11-04 16:33:04 -0500

    Slight updates to personalize starter website.

--- src/components/layout.js
+++ src/components/layout.js
@@ -26,19 +26,12 @@ const Layout = ({ children }) => {
   return (
     <>
       <Header siteTitle={data.site.siteMetadata.title} />
-      <div
-        style={{
-          margin: `0 auto`,
-          maxWidth: 960,
-          padding: `0px 1.0875rem 1.45rem`,
-          paddingTop: 0,
-        }}
-      >
+      <div className="container">
         <main>{children}</main>
         <footer>
-          © {new Date().getFullYear()}, Built with
+          © {new Date().getFullYear()}, Website Coutesy of 
           {` `}
-          <a href="https://www.gatsbyjs.org">Gatsby</a>
+          <a href="https://www.marcusquettan.com">Marcus Quettan</a>
         </footer>
       </div>
     </>
--- src/pages/index.js
+++ src/pages/index.js
@@ -8,13 +8,12 @@ import SEO from "../components/seo"
 const IndexPage = () => (
   <Layout>
     <SEO title="Home" />
-    <h1>Hi people</h1>
-    <p>Welcome to your new Gatsby site.</p>
-    <p>Now go build something great.</p>
+    <h1>Hello World!</h1>
+    <p>Welcome to Saurel Quettan Enterprises' new site.</p>
+    <p>We are currently under construction.  Please, check back soon for new features!</p>
     <div style={{ maxWidth: `300px`, marginBottom: `1.45rem` }}>
-      <Image />
+      {/* <Image /> */}
     </div>
-    <Link to="/page-2/">Go to page 2</Link>
   </Layout>
 )
 
