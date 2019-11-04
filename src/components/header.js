4e132ba52476bcfa2c6c1922db2a31145b18d1db
Marcus Quettan <marcus.quettan@gtri.gatech.edu>
2019-11-04 16:25:27 -0500

    Adding navigation bar with animated hamburger link.

--- src/components/header.js
+++ src/components/header.js
@@ -1,23 +1,35 @@
 import { Link } from "gatsby"
 import PropTypes from "prop-types"
-import React from "react"
+import React, { useState } from "react"
 import "../styles/main.scss"
 
-const Header = ({ siteTitle }) => (
-  <header className="header">
-    <div className="container header-wrapper">
-      <h1 className="title">
-        <Link to="/">{siteTitle}</Link>
-      </h1>
-      <ul className="navigation">
-        <li><Link to="/">Home</Link></li>
-        <li><Link to="/about">About</Link></li>
-        <li><Link to="/blog">Blog</Link></li>
-        <li><Link to="/events">Events</Link></li>
-      </ul>
-    </div>
-  </header>
-)
+export default function Header( {siteTitle} ) {
+  const [menuIconClicked, setMenuIconClicked] = useState(false);
+
+  return (
+    <header className="header">
+      <div className="container header-wrapper">
+        <h1 className="title">
+          <Link to="/">{siteTitle}</Link>
+        </h1>
+        <div className="navigation-row">
+          <ul className="navigation-list">
+            <li><Link to="/">Home</Link></li>
+            <li><Link to="/about">About</Link></li>
+            <li><Link to="/blog">Blog</Link></li>
+            <li><Link to="/events">Events</Link></li>
+          </ul>
+        
+          <div className={"hamburger-icon " + (menuIconClicked ? "change" : "")} onClick={() => setMenuIconClicked(!menuIconClicked)}>
+            <div class="bar1"></div>
+            <div class="bar2"></div>
+            <div class="bar3"></div>
+          </div>
+        </div>
+      </div>
+    </header>
+  )
+}
 
 Header.propTypes = {
   siteTitle: PropTypes.string,
@@ -26,5 +38,3 @@ Header.propTypes = {
 Header.defaultProps = {
   siteTitle: ``,
 }
-
-export default Header
