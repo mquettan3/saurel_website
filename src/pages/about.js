e8b318d592ee4dcd29b226380c73a87ac249f487
Marcus Quettan <mquettan@gmail.com>
2019-11-10 13:30:17 -0500

    Updating about page.  Adding approach page.

---
+++ src/pages/about.js
@@ -0,0 +1,12 @@
+import React from 'react'
+
+export default function AboutPage() {
+    return (
+        <div>
+            <h1>About Saurel Business</h1>
+            <p>
+                
+            </p>
+        </div>
+    )
+}
---
+++ src/pages/approach.js
@@ -0,0 +1,14 @@
+import React from 'react'
+
+export default function approach() {
+    return (
+        <div>
+            Aliquip id irure id proident dolore. Velit exercitation fugiat sint reprehenderit eiusmod ullamco id eu elit. Lorem voluptate enim laborum culpa esse reprehenderit eiusmod occaecat occaecat reprehenderit non anim exercitation. Aliquip incididunt deserunt mollit labore. Ea proident reprehenderit est Lorem eiusmod mollit.
+            Dolore ad aliquip anim cillum aliqua Lorem ad nulla culpa eiusmod consequat in velit aliqua. Quis aliqua labore excepteur laboris duis ullamco anim adipisicing sit sunt magna dolor. Lorem cillum amet incididunt velit ullamco enim sunt deserunt culpa qui incididunt. Do magna nulla mollit consectetur ea officia reprehenderit.
+            Veniam tempor in et exercitation adipisicing cillum Lorem Lorem sint proident commodo qui elit. Ullamco nulla duis tempor do voluptate amet ullamco commodo consectetur dolor reprehenderit. Culpa mollit pariatur quis deserunt cillum elit.
+            Eu do aute nulla veniam ipsum velit esse aliquip sunt dolore consequat sint. Nostrud dolore id aliqua laborum reprehenderit id occaecat deserunt ad aute quis quis cillum ex. Adipisicing elit commodo irure nostrud nostrud exercitation sunt quis officia officia ullamco quis ut cillum.
+            Anim elit laborum esse velit aute laboris sint qui amet. Culpa proident ut irure exercitation occaecat do dolor culpa anim dolore. Officia mollit reprehenderit excepteur labore ut aliquip.
+            Commodo officia id dolor culpa ex nisi. Sint laborum proident eiusmod nostrud sit qui esse do sit enim exercitation do quis commodo. Occaecat laboris fugiat et mollit dolor. Pariatur qui nostrud et est eu sunt est Lorem. Ad ullamco sunt laboris Lorem veniam eiusmod aliqua quis reprehenderit sit quis excepteur sunt consequat. Excepteur incididunt elit ad ad laborum magna elit duis deserunt.
+        </div>
+    )
+}
